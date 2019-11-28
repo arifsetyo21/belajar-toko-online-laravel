8d879feb075edd87795f0eca65e9ed073124c980
arifsetyo21 <arifsetyo19@gmail.com>
2019-11-28 16:39:47 +0700

    fix conflict

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate as GateContract;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,17 +21,9 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(GateContract $gate)
+    public function boot()
     {
-        $this->registerPolicies($gate);
+        $this->registerPolicies();
 
-        // $gate->define('admin-access', function($user) {
-        //     return $user->role == 'admin';
-        // });
-
-        // $gate->define('customer-access', function($user) {
-        //     return $user->role == 'customer';
-        // });
-        // //
     }
 }
